88906261589d0d073806280275bc96519b5ac3cd
nf-projects <90344282+nf-projects@users.noreply.github.com>
2025-01-11 17:45:20 -0800

    strip chat msg formatting

--- bukkit/src/main/java/net/mcmetrics/plugin/listeners/ChatMessageListener.java
+++ bukkit/src/main/java/net/mcmetrics/plugin/listeners/ChatMessageListener.java
@@ -2,6 +2,7 @@ package net.mcmetrics.plugin.listeners;
 
 import net.mcmetrics.plugin.MCMetricsSpigotPlugin;
 import net.mcmetrics.shared.models.ChatMessage;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -15,17 +16,26 @@ public class ChatMessageListener implements Listener {
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-        if (event.isCancelled()) return;
-        
+        if (event.isCancelled())
+            return;
+
         ChatMessage chatMessage = new ChatMessage();
         chatMessage.player_uuid = event.getPlayer().getUniqueId();
         chatMessage.player_username = event.getPlayer().getName();
-        chatMessage.message = event.getMessage();
+
+        // Strip all color codes and formatting
+        String strippedMessage = ChatColor.stripColor(event.getMessage());
+        // Also strip any remaining & color codes that haven't been translated yet
+        strippedMessage = strippedMessage.replaceAll("&[0-9a-fk-orA-FK-OR]", "");
+        // Remove any excessive whitespace
+        strippedMessage = strippedMessage.trim().replaceAll("\\s+", " ");
+
+        chatMessage.message = strippedMessage;
 
         plugin.getApi().insertChatMessage(chatMessage)
-            .exceptionally(throwable -> {
-                plugin.getLogger().warning("Failed to record chat message: " + throwable.getMessage());
-                return null;
-            });
+                .exceptionally(throwable -> {
+                    plugin.getLogger().warning("Failed to record chat message: " + throwable.getMessage());
+                    return null;
+                });
     }
 }
